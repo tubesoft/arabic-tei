bf67166a170a71c5b358a22482f22c75ce8aea09
Takatomo INOUE <inotazo@gmail.com>
2025-07-11 20:55:50 +0900

    Replace build files again

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/arabic-tei/',
+  base: './',
   build: {
     outDir: 'docs'
   },
